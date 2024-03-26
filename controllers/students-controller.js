31a896d7a0dcd5509d830be05d3f003615ca4677
svetlana vasileva <svetlana.vasileva@afd.co.uk>
2024-03-26 06:55:22 +0000

    title updated

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry",
+      msg: "Welcome to Students Registry App",
       students: students
     };
     res.render('home', model);
